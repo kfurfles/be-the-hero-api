241637888a725499a104f655bbda1744aeeab6de
kfurfles <kelvinsilva.ksv@gmail.com>
2020-04-08 22:26:37 -0300

    wip

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,10 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+routes.get('/', (req,res) =>{
+    return res.send(`Be the Hero Api, Hello Folks`)
+});
+
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
